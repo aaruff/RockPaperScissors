e1d5c8e7834d988995151bf31dd8aa2258cfb68a
Anwar A. Ruff <anwarruff@gmail.com>
2022-11-10 16:02:53 -0500

    Added rock, paper, scissors buttons

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -7,13 +7,52 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    let outcomeStates = ["Win", "Lose"]
+    let moves = ["Rock", "Paper", "Scissors"]
+    
+    @State var choice = Int.random(in: 0..<3)
+    @State var outcome = Int.random(in: 0..<2)
+    @State var move = 0
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("If the computer chooses **\(moves[choice])**")
+            Text("What do you play to **\(outcomeStates[outcome])** the game?")
+            VStack {
+                Button(
+                    role: .none,
+                    action: {},
+                    label: {
+                        Image("fist")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width:100)
+                        
+                    }
+                    )
+                Button(
+                    role: .none,
+                    action: {},
+                    label: {
+                        Image("open-hand")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width:100)
+                    }
+                    )
+                Button(
+                    role: .none,
+                    action: {},
+                    label: {
+                        Image("peace")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width:100)
+                    }
+                    )
+                
+            }
         }
         .padding()
     }
