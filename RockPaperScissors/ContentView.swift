0b53b8ff9caab15c9557af486dd20ed65749044b
Anwar A. Ruff <anwarruff@gmail.com>
2022-11-13 11:17:05 -0500

    Added move button view and struct

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -7,6 +7,40 @@
 
 import SwiftUI
 
+enum Outcome {
+    case win, lose
+}
+
+enum Move {
+    case rock, paper, scissors
+}
+
+struct MoveButton: View {
+    var imageName: String
+    var borderWidth: CGFloat
+    var borderColor: Color
+    var action: () -> Void
+    
+    var body: some View {
+                Button(
+                    role: .none,
+                    action: {},
+                    label: {
+                        Image(imageName)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width:100)
+                        
+                    }
+                )
+                .padding()
+                .overlay(
+                    RoundedRectangle(cornerRadius: 40)
+                        .stroke(borderColor, lineWidth: borderWidth))
+        
+    }
+}
+
 
 struct ContentView: View {
     let outcomeStates = ["Win", "Lose"]
@@ -20,38 +54,17 @@ struct ContentView: View {
             Text("If the computer chooses **\(moves[choice])**")
             Text("What do you play to **\(outcomeStates[outcome])** the game?")
             VStack {
-                Button(
-                    role: .none,
-                    action: {},
-                    label: {
-                        Image("fist")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width:100)
-                        
-                    }
-                    )
-                Button(
-                    role: .none,
-                    action: {},
-                    label: {
-                        Image("open-hand")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width:100)
-                    }
-                    )
-                Button(
-                    role: .none,
-                    action: {},
-                    label: {
-                        Image("peace")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width:100)
-                    }
-                    )
+                MoveButton(imageName: "closed-fist", borderWidth: 2, borderColor: Color.gray) {
+                   if (
+                }
                 
+                MoveButton(imageName: "palm", borderWidth: 2, borderColor: Color.gray){
+                    
+                }
+                
+                MoveButton(imageName: "victory-2", borderWidth: 2, borderColor: Color.gray){
+                    
+                }
             }
         }
         .padding()
